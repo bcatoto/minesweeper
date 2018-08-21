bebed9bfcb7c5140970cec476beaf29a19e981dc
Bianca Catoto <biancacatoto@biancas-macbook-pro-6.local>
2018-08-21 16:14:56 +0800

    Initial Commit

---
+++ Minesweeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Minesweeper
+//
+//  Created by Bianca Catoto on 8/21/18.
+//  Copyright © 2018 Bianca Catoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
